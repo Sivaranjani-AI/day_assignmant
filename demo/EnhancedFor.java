5231a91501a8a6cdedf019e40577adcf32a03db6
Sivaranjani-AI <sivaranjani20032006@gmail.com>
2026-06-06 22:28:08 +0530

    Add files via upload

---
+++ demo/EnhancedFor.java
@@ -0,0 +1,8 @@
+public class EnhancedFor {
+    public static void main(String[] args) {
+        String[] arr = {"Apple","Mango","Banana","Cherry"};
+        for (String num:arr) {
+            System.out.println(num);
+        }
+    }
+}
---
+++ demo/HollowHourglass.java
@@ -0,0 +1,38 @@
+public class HollowHourglass {
+    public static void main(String[] args) {
+
+        int n = 5;
+
+        for(int i = n; i >= 1; i--) {
+
+            for(int s = 1; s <= n-i; s++)
+                System.out.print(" ");
+
+            for(int j = 1; j <= 2*i-1; j++) {
+
+                if(i == n || j == 1 || j == 2*i-1)
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            }
+
+            System.out.println();
+        }
+
+        for(int i = 2; i <= n; i++) {
+
+            for(int s = 1; s <= n-i; s++)
+                System.out.print(" ");
+
+            for(int j = 1; j <= 2*i-1; j++) {
+
+                if(i == n || j == 1 || j == 2*i-1)
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            }
+
+            System.out.println();
+        }
+    }
+}
