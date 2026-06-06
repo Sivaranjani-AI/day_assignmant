99041fad38ceb4d4dee5037d2fc59f4468764b2a
Sivaranjani-AI <sivaranjani20032006@gmail.com>
2026-06-06 20:56:04 +0530

    Add files via upload

---
+++ demo/Armstrong.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Armstrong{
+    public static void main(String [] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter a number:");
+        int n = sc.nextInt();
+        int num = n ;
+        int sum = 0;
+        while (n > 0) {
+          int d = n % 10;
+          sum = sum+ (d*d*d);
+          n= n / 10;
+        }
+        if (sum==num){
+            System.out.println(num + "Armstrong Number");
+        } else {
+            System.out.println(num + "Not An Armstrong Number");
+        }
+    }
+}
--- demo/pattern1.java
+++ demo/pattern1.java
@@ -1,7 +1,7 @@
-public class Pattern1{
-    public static void main (string[] args){
-        for(int i=1;i<=5,i++){
-            for(int j=1;j<=i,j++){
+public class Pattern1 {
+    public static void main (String[] args){
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=i;j++){
                 if((i+j)%2==0)
                 System.out.print("1");
                 else
