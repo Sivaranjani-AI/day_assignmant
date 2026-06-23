e1c5701c313d0bf82101bfb05aa5afd40dde9d02
Sivaranjani-AI <sivaranjani20032006@gmail.com>
2026-06-23 19:56:30 +0530

    Add files via upload

--- demo/Example.java
+++
@@ -1,9 +0,0 @@
-public class Example {
-    static void sayHello(){
-        System.out.println("Hello,world!");
-    }
-
-    public static void main(String[] args) {
-        sayHello();
-    }
-}
