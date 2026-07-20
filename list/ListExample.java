5825ccaa667c3f45632036edbc48e443a19fd0e5
Sivaranjani-AI <sivaranjani20032006@gmail.com>
2026-07-20 11:27:31 +0530

    Add files via upload

---
+++ list/ListExample.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class ListExample {
+    public static void main(String[] args) {
+        List<String> fruits = new ArrayList<>();
+
+        fruits.add("Apple");
+        fruits.add("Banana");
+        fruits.add("Orange");
+        fruits.add("Banana");
+
+        System.out.println(fruits);
+    }
+}
